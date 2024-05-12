debd25fd34b46382a4dc66debbacc83567e31a6c
Dave Molk <davemolk@gmail.com>
2024-05-11 19:39:23 -0600

    finish adding comments functionality to lobsters

--- lobsters/src/client.rs
+++ lobsters/src/client.rs
@@ -1,5 +1,5 @@
 use core::fmt;
-use std::fmt::format;
+use std::collections::{self, HashMap};
 
 use serde_derive::Deserialize;
 use anyhow::Result;
@@ -22,7 +22,6 @@ struct Post {
     title: String,
     url: String,
     comment_count: u32,
-    comments_url: String,
     tags: Vec<String>,
 }
 
@@ -33,16 +32,16 @@ impl fmt::Display for Post {
             "Title: {}\n\
             URL: {}\n\
             Tags: {:?}\n\
+            Comment Count: {}\n\
             ID: {}\n\
-            Num Comments: {}\n\
-            Comments URL: {}\n\
-            ", self.title, self.url, self.tags, self.short_id, self.comment_count, self.comments_url
+            ", self.title, self.url, self.tags, self.comment_count, self.short_id
         )
     }
 }
 
 #[derive(Debug, Deserialize)]
 struct CommentsResponse {
+    title: String,
     comments: Vec<Comment>,
 }
 
@@ -76,20 +75,46 @@ impl LobsterClient {
             url = URL_HOTTEST;
         }
         let resp = self.get_lobsters(url)?;
-        let mut short_ids = Vec::new();
-        for post in resp {
-            short_ids.push(post.short_id.clone());
+        for post in &resp {
             println!("{post}");
         }
+        // check if user wants to see comments
         println!("enter an id to see the comments, or press any key to quit");
         let mut input = String::new();
         std::io::stdin().read_line(&mut input)?;
-        if !short_ids.iter().any(|e| input.contains(e)) {
+        input = input.trim().to_string();
+        if !resp.iter().any(|e| input == e.short_id) {
             return Ok(())
         }
+        println!();
+        println!();
+        let mut map: HashMap<&str, usize> = collections::HashMap::new();
         let comment_url = format!("https://lobste.rs/s/{}.json", input);
-        let comments = self.get_comments(&comment_url)?;
-        dbg!(comments);
+        let resp = self.get_comments(&comment_url)?;
+        println!("{} comments:", resp.title);
+        for comment in &resp.comments {
+            match &comment.parent_comment {
+                None => {
+                    map.entry(&comment.short_id).or_default();
+                },
+                Some(parent) => {
+                    // subsequent comments will always have a parent (or be a parent)
+                    let v = map.get(parent.as_str()).unwrap();
+                    // need to add extra tab for each child-level
+                    map.insert(&comment.short_id, *v+1);
+                },
+                
+            }
+        }
+        // go through resp.comments, have prefix as repeated \t
+        for comment in &resp.comments {
+            // we know elements are in the map, so can unwrap
+            let value = map.get(comment.short_id.as_str()).unwrap();
+            let prefix = "\t".repeat(*value);
+            // close enough, keeps it in the same tab-level
+            let c = comment.comment_plain.replace("\r\n\r\n", " ");
+            println!("\n{prefix}* {}", c);
+        }
         Ok(())
     }
     fn get_lobsters(&self, url: &str) -> Result<ApiResponse> {
