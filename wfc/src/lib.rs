5c0b96e8b6bcedb6b873034b1313750674751ed5
Dave Molk <davemolk@gmail.com>
2024-06-21 19:01:06 -0600

    add tests to word frequency counter

--- wfc/src/lib.rs
+++ wfc/src/lib.rs
@@ -1,19 +1,26 @@
 use std::fs;
 use std::collections::HashMap;
+use std::io::{self, Write};
 use regex::Regex;
 use anyhow::{anyhow, Result, Context};
 
-pub fn find_frequencies(args: Vec<String>) -> Result<()> {
+pub fn run(args: Vec<String>) -> Result<()> {
     if args.len() != 2 {
         return Err(anyhow!("not enough arguments provided, need a file name"));
     }
+    let content = fs::read_to_string(&args[1]).context("failed to read file")?;
+    count_words(&mut io::stdout(), content)?;
+    Ok(())
+}
+
+fn count_words(output: &mut dyn Write, mut content: String) -> Result<()> {
+    content = content.to_lowercase();
     // get rid of punctuation, numbers, etc
     let re = Regex::new(r"[^a-z\s]")?;
+    content = re.replace_all(&content, "").to_string();
+
     let mut counts: HashMap<&str, u64> = HashMap::new();
-    let mut file_contents = fs::read_to_string(&args[1]).context("failed to read file")?;
-    file_contents = file_contents.to_lowercase();
-    file_contents = re.replace_all(&file_contents, "").to_string();
-    for line in file_contents.lines() {
+    for line in content.lines() {
         for word in line.split_whitespace() {
             counts.entry(word).and_modify(|w| *w += 1).or_insert(1);
         }
@@ -21,8 +28,52 @@ pub fn find_frequencies(args: Vec<String>) -> Result<()> {
     let mut count_vec: Vec<_> = counts.iter().collect();
     count_vec.sort_by(|a, b| b.1.cmp(a.1));
     for (k, v) in count_vec {
-        println!("{}: {}", k, v);
+        writeln!(output, "{}: {}", k, v)?;
     }
     Ok(())
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn count_words_is_case_insensitive() {
+        let content = "foo Foo fOO".to_owned();
+        let mut output = Vec::<u8>::new();
+        assert!(count_words(&mut output, content).is_ok());
+        let got = String::from_utf8_lossy(&output);
+        assert_eq!(got, "foo: 3\n".to_owned());
+    }
+    #[test]
+    fn count_empty_string() {
+        let content = "".to_owned();
+        let mut output = Vec::<u8>::new();
+        assert!(count_words(&mut output, content).is_ok());
+        let got = String::from_utf8(output).unwrap();
+        assert_eq!(got, "".to_string());
+    }
+    #[test]
+    fn strips_punctuation_and_numbers() {
+        let content = "fo1o !foo foo.".to_owned();
+        let mut output = Vec::<u8>::new();
+        assert!(count_words(&mut output, content).is_ok());
+        let got = String::from_utf8(output).unwrap();
+        assert_eq!(got, "foo: 3\n".to_owned());
+    }
+    #[test]
+    fn strips_whitespace_stuff() {
+        let content = "     foo\t\nfoo\n foo\t".to_owned();
+        let mut output = Vec::<u8>::new();
+        assert!(count_words(&mut output, content).is_ok());
+        let got = String::from_utf8(output).unwrap();
+        assert_eq!(got, "foo: 3\n".to_owned());
+    }
+    #[test]
+    fn counts_multiple_words() {
+        let content = "foo bar bar".to_owned();
+        let mut output = Vec::<u8>::new();
+        assert!(count_words(&mut output, content).is_ok());
+        let got = String::from_utf8(output).unwrap();
+        assert_eq!(got, "bar: 2\nfoo: 1\n".to_owned());
+    }
+}
--- wfc/src/main.rs
+++ wfc/src/main.rs
@@ -1,8 +1,8 @@
-use wfc::find_frequencies;
+use wfc::run;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if let Err(e) = find_frequencies(args) {
+    if let Err(e) = run(args) {
         eprintln!("failed to find frequencies: {}", e);
         std::process::exit(1);
     }
